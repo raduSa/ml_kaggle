8a0347dc22861404c453133dde5835416ded895c
raduSa <radusavin2004@gmail.com>
2025-05-30 22:21:20 +0300

    anotha one

--- CNN.py
+++ CNN.py
@@ -10,6 +10,7 @@ from keras.src.layers import MaxPooling2D
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 import tensorflow as tf
+from tensorflow.keras.layers import BatchNormalization
 
 # test1 - 0.64 x 0.9315 -> overfit
 model_1 = Sequential([
@@ -70,6 +71,34 @@ model_3 = Sequential([
     Dense(5, activation='softmax'),
 ])
 
+model_4 = Sequential([
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    BatchNormalization(),
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    BatchNormalization(),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    BatchNormalization(),
+    Dropout(0.2),
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    BatchNormalization(),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Flatten(),
+
+    Dense(128, activation='relu'),
+    Dense(100, activation='relu'),
+    Dropout(0.2),
+    Dense(5, activation='softmax'),
+])
+
 
 def load_data(csv_path, image_dir):
     df = pd.read_csv(csv_path)
@@ -90,7 +119,7 @@ def build_model(model):
 
 early_stopping = EarlyStopping(
     monitor='val_loss',
-    patience=3,
+    patience=5,
     restore_best_weights=True
 )
 
@@ -104,10 +133,10 @@ Y_train = Y_train.reshape(len(Y_train), 1)
 X_val = X_val.reshape(len(X_val), 100, 100, 3)
 Y_val = Y_val.reshape(len(Y_val), 1)
 
-# Preprocessing (subtract mean)
+'''# Preprocessing (subtract mean)
 mean = X_train.mean(axis=0)
 X_train = X_train - mean
-X_val = X_val - mean
+X_val = X_val - mean'''
 
 Y_train = to_categorical(Y_train, num_classes=5)
 Y_val = to_categorical(Y_val, num_classes=5)
@@ -122,12 +151,9 @@ batch_size = 64
 
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,              # Normalize pixel values
-    rotation_range=20,           # Random rotation in degrees
-    width_shift_range=0.1,       # Horizontal shift (10%)
-    height_shift_range=0.1,      # Vertical shift (10%)
-    zoom_range=0.1,              # Random zoom
+    shear_range=0.2,
+    zoom_range=0.2,
     horizontal_flip=True,        # Random horizontal flip
-    fill_mode='nearest'          # Fill in newly created pixels
 )
 test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
@@ -135,13 +161,14 @@ train_generator = train_datagen.flow(X_train, Y_train, batch_size=batch_size)
 validation_generator = test_datagen.flow(X_val, Y_val, batch_size=batch_size)
 
 # build model
-model = build_model(model_3)
+model = build_model(model_4)
 
 model.fit(
         train_generator,
         epochs=50,
         validation_data=validation_generator,
-        callbacks=[early_stopping])
+        callbacks=[early_stopping]
+)
 
-model.evaluate(X_val, Y_val)
-model.save_weights('first_try.h5')  # always save your weights after training or during training
+model.evaluate(X_val / 255.0, Y_val / 255.0)
+model.save_weights('first_try.weights.h5')
