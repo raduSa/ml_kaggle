b1467ee981e55196f79aacbf9601d929b98db1aa
raduSa <radusavin2004@gmail.com>
2025-05-31 01:49:12 +0300

    hello

---
+++ CNN_random_hyperparam.py
@@ -0,0 +1,168 @@
+import os
+
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D
+from keras.optimizers import Adam
+import numpy as np
+from PIL import Image
+from keras.src.layers import MaxPooling2D, GlobalAveragePooling2D
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+from tensorflow.keras.layers import BatchNormalization
+
+# / added more layers, couldn't overfit
+model_3 = Sequential([
+    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(100, 100, 3)),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Conv2D(64, (3, 3), padding='same', activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Conv2D(64, (3, 3), padding='same', activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Conv2D(64, (3, 3), padding='same', activation='relu'),
+    MaxPooling2D((2, 2)),
+    Dropout(0.2),
+
+    Flatten(),
+
+    Dense(128, activation='relu'),
+    Dense(100, activation='relu'),
+    Dropout(0.2),
+    Dense(5, activation='softmax'),
+])
+
+
+def load_data(csv_path, image_dir):
+    df = pd.read_csv(csv_path)
+    X, y = [], []
+    for _, row in df.iterrows():
+        file_name = row[0].strip() + ".png"
+        label = row[1]
+        img_path = os.path.join(image_dir, file_name)
+        img = Image.open(img_path).convert("RGB")
+        img_array = np.array(img).flatten()
+        X.append(img_array)
+        y.append(label)
+    return np.array(X), np.array(y)
+
+def build_model(model):
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    restore_best_weights=True
+)
+
+# Load training and validation data
+X_train, Y_train = load_data("train.csv", "train")
+X_val, Y_val = load_data("validation.csv", "validation")
+
+X_train = X_train.reshape(len(X_train), 100, 100, 3)
+Y_train = Y_train.reshape(len(Y_train), 1)
+
+X_val = X_val.reshape(len(X_val), 100, 100, 3)
+Y_val = Y_val.reshape(len(Y_val), 1)
+
+'''# Preprocessing (subtract mean)
+mean = X_train.mean(axis=0)
+X_train = X_train - mean
+X_val = X_val - mean'''
+
+Y_train = to_categorical(Y_train, num_classes=5)
+Y_val = to_categorical(Y_val, num_classes=5)
+
+print("Shape of X_train:", X_train.shape)
+print("Shape of y_train:", Y_train.shape)
+print("Shape of X_val:", X_val.shape)
+print("Shape of y_val:", Y_val.shape)
+
+# Prepare data
+batch_size = 32
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,              # Normalize pixel values
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,        # Random horizontal flip
+)
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow(X_train, Y_train, batch_size=batch_size)
+validation_generator = test_datagen.flow(X_val, Y_val, batch_size=batch_size)
+
+# build model
+# model = build_model(model_3)
+#
+# model.fit(
+#         train_generator,
+#         epochs=50,
+#         validation_data=validation_generator,
+#         callbacks=[early_stopping]
+# )
+#
+# #model.fit(X_train, Y_train, batch_size=32, epochs=50, validation_data=(X_val, Y_val), callbacks=[early_stopping])
+#
+# model.evaluate(X_val / 255.0, Y_val / 255.0)
+# model.save_weights('snd.weights.h5')
+
+import keras_tuner as kt
+
+def build_model(hp):
+    model = Sequential()
+    model.add(Conv2D(
+        filters=hp.Choice('filters_1', [32, 64, 128]),
+        kernel_size=hp.Choice('kernel_size_1', [3, 5]),
+        activation='relu',
+        input_shape=(100, 100, 3),
+        padding='same'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Dropout(hp.Float('dropout_1', 0.2, 0.5, step=0.1)))
+
+    model.add(Conv2D(
+        filters=hp.Choice('filters_2', [64, 128, 256]),
+        kernel_size=hp.Choice('kernel_size_2', [3, 5]),
+        activation='relu',
+        padding='same'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Dropout(hp.Float('dropout_2', 0.2, 0.5, step=0.1)))
+
+    model.add(Conv2D(
+        filters=hp.Choice('filters_3', [64, 128, 256]),
+        kernel_size=3,
+        activation='relu',
+        padding='same'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Dropout(hp.Float('dropout_3', 0.2, 0.5, step=0.1)))
+
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(
+        hp.Int('dense_units', 64, 256, step=64),
+        activation='relu'))
+    model.add(Dropout(hp.Float('dropout_dense', 0.3, 0.6, step=0.1)))
+    model.add(Dense(5, activation='softmax'))
+
+    model.compile(
+        optimizer=Adam(
+            learning_rate=hp.Choice('lr', [1e-2, 1e-3, 1e-4])),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+
+    return model
+
+tuner = kt.RandomSearch(
+    build_model,
+    objective='val_accuracy',
+    max_trials=200,  # Try 20 different combinations
+    directory='tuning_dir',
+    project_name='animal_classifier')
+
+tuner.search(train_generator, validation_data=validation_generator, epochs=1, callbacks=[early_stopping])
